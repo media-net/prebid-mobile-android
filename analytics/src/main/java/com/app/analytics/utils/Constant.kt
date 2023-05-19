61cef1c528d390c8fe08668719d22d505a59d120
Tushar Baid <baidtushar11@gmail.com>
2023-05-19 16:27:08 +0530

    Misc changes

--- analytics/src/main/java/com/app/analytics/utils/Constant.kt
+++ analytics/src/main/java/com/app/analytics/utils/Constant.kt
@@ -5,5 +5,9 @@ object Constant {
     object Providers {
         const val PROVIDER_DEFAULT = "Default"
         const val PROVIDER_DEBUG = "Debug"
+        const val DEFAULT_UGD_VALUE = "3"
+        const val DEFAULT_DTYPE_ID_VALUE = "3"
+        const val MOBILE_SDK = "MOBILE_SDK"
+        const val DEFAULT_TO_CONSIDER_VALUE = "1"
     }
 }
